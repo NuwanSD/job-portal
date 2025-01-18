397a57a186714f6aa9ddca7a60a19b626f053e99
NuwanSD <sagaradnuwan@gmail.com>
2025-01-18 16:08:40 +0530

    Added new model posted jobs

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ require("dotenv").config();
 const jobSeekerRoutes = require("./routes/jobSeekerRoutes");
 const recruiterRoutes = require("./routes/recruiterRoutes");
 const jobRoutes = require("./routes/jobRoutes");
+const postedJobRoutes = require("./routes/postedJobRoutes");
 
 const app = express();
 
@@ -20,6 +21,7 @@ app.get("/", (req, res) => {
 app.use(jobSeekerRoutes);
 app.use(recruiterRoutes);
 app.use(jobRoutes);
+app.use(postedJobRoutes);
 
 //Start server
 const PORT = process.env.PORT || 3000;
