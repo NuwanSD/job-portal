a09d1b4aa47ce773c41c0d726c260f535270719d
NuwanSD <sagaradnuwan@gmail.com>
2025-01-24 15:34:49 +0530

    updated router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+} from "react-router-dom";
 
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 import Home from "./pages/home/Home";
-
 import Candidate from "./pages/candidates/Candidate";
 import Recruiter from "./pages/recruiters/Recruiter";
 import Contact from "./pages/contact/Contact";
@@ -13,55 +17,45 @@ import Job from "./pages/FindJobs/Job";
 import JobDetail from "./pages/FindJobs/JobDetail";
 import RecruiterProfile from "./pages/recruiters/RecruiterProfile";
 import CandidateProfile from "./pages/candidates/CandidateProfile";
+import Login from "./auth/login";
+import SignUp from "./auth/SignUp";
+import NotFound from "./components/NotFound";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home />, errorElement: <div>404 Not Found</div> },
-  {
-    path: "/job",
-    element: <Job />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/job/:jobId",
-    element: <JobDetail />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/candidate",
-    element: <Candidate />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/candidate/:id",
-    element: <CandidateProfile />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/recruiters",
-    element: <Recruiter />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/recruiters/:recruiterId",
-    element: <RecruiterProfile />,
-    errorElement: <div>404 Not Found</div>,
-  },
-  {
-    path: "/contact",
-    element: <Contact />,
-    errorElement: <div>404 Not Found</div>,
-  },
-]);
+const AppContent = () => {
+  const location = useLocation();
+  const noNavbarFooterPaths = ["/login", "/signUp"];
 
-const App = () => {
   return (
     <div id="root">
       <div className="main">
-        <Navbar />
-        <RouterProvider router={router} />
+        {!noNavbarFooterPaths.includes(location.pathname) && <Navbar />}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/job" element={<Job />} />
+          <Route path="/job/:jobId" element={<JobDetail />} />
+          <Route path="/candidate" element={<Candidate />} />
+          <Route path="/candidate/:id" element={<CandidateProfile />} />
+          <Route path="/recruiters" element={<Recruiter />} />
+          <Route
+            path="/recruiters/:recruiterId"
+            element={<RecruiterProfile />}
+          />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signUp" element={<SignUp />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
       </div>
-      <Footer />
+      {!noNavbarFooterPaths.includes(location.pathname) && <Footer />}
     </div>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <AppContent />
+    </Router>
   );
 };
 
