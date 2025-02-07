f16b15f383c217c043475b77cf63a8671a60e087
NuwanSD <sagaradnuwan@gmail.com>
2025-02-07 21:53:53 +0530

    updated

--- client/src/pages/dashboard/Dashboard.jsx
+++ client/src/pages/dashboard/Dashboard.jsx
@@ -1,102 +1,41 @@
 import React, { useState } from "react";
-import PropTypes from "prop-types";
-import Tabs from "@mui/material/Tabs";
-import Tab from "@mui/material/Tab";
-import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
-import { Container } from "@mui/material";
 
 import Overview from "./components/Overview";
 import AppliedJob from "./components/AppliedJobs";
 import FavoriteJobs from "./components/FavoriteJobs";
 import JobAlert from "./components/JobAlert";
 import Setting from "./components/Settings";
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props;
-
-  return (
-    <div
-      role="tabpanel"
-      hidden={value !== index}
-      id={`vertical-tabpanel-${index}`}
-      aria-labelledby={`vertical-tab-${index}`}
-      {...other}
-    >
-      {value === index && (
-        <Box sx={{ p: 3 }}>
-          <Typography>{children}</Typography>
-        </Box>
-      )}
-    </div>
-  );
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-};
-
-function a11yProps(index) {
-  return {
-    id: `vertical-tab-${index}`,
-    "aria-controls": `vertical-tabpanel-${index}`,
-  };
-}
+import Sidebar from "./components/Sidebar";
+import { Container, Divider } from "@mui/material";
 
 const Dashboard = () => {
-  const [value, setValue] = useState(0);
+  const [selectedTab, setSelectedTab] = useState("Overview");
 
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
+  const renderContent = () => {
+    switch (selectedTab) {
+      case "Overview":
+        return <Overview />;
+      case "Applied Jobs":
+        return <AppliedJob />;
+      case "Favorite Jobs":
+        return <FavoriteJobs />;
+      case "Job Alert":
+        return <JobAlert />;
+      case "Settings":
+        return <Setting />;
+      default:
+        return <Overview />;
+    }
   };
+
   return (
-    <Container>
-      <Box
-        sx={{
-          flexGrow: 1,
-          bgcolor: "background.paper",
-          display: "flex",
-          height: "100vh",
-        }}
-      >
-        <Tabs
-          orientation="vertical"
-          variant="scrollable"
-          value={value}
-          onChange={handleChange}
-          aria-label="Vertical tabs example"
-          sx={{
-            borderRight: 1,
-            borderColor: "divider",
-            py: 10,
-          }}
-        >
-          <Tab label="Overview" {...a11yProps(0)} />
-          <Tab label="Appied Jobs" {...a11yProps(1)} />
-          <Tab label="Favorite Jobs" {...a11yProps(2)} />
-          <Tab label="Job Alert" {...a11yProps(3)} />
-          <Tab label="Settings" {...a11yProps(4)} />
-        </Tabs>
-        <Box sx={{ width: "100%" }}>
-          <TabPanel value={value} index={0}>
-            <Overview />
-          </TabPanel>
-          <TabPanel value={value} index={1}>
-            <AppliedJob />
-          </TabPanel>
-          <TabPanel value={value} index={2}>
-            <FavoriteJobs />
-          </TabPanel>
-          <TabPanel value={value} index={3}>
-            <JobAlert />
-          </TabPanel>
-          <TabPanel value={value} index={4}>
-            <Setting />
-          </TabPanel>
-        </Box>
+    <Container sx={{ display: "flex", gap: 5 }}>
+      <Box>
+        <Sidebar setSelectedTab={setSelectedTab} />
       </Box>
+
+      <Box sx={{ flexGrow: 1 }}>{renderContent()}</Box>
     </Container>
   );
 };
