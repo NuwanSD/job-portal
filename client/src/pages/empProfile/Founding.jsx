5c53622e585307835c388fee704073c8bb277628
NuwanSD <sagaradnuwan@gmail.com>
2025-02-22 09:09:58 +0530

    updated with schemas

--- client/src/pages/empProfile/Founding.jsx
+++ client/src/pages/empProfile/Founding.jsx
@@ -3,16 +3,26 @@ import { Typography, Divider, Box, TextField, Button } from "@mui/material";
 import { useForm, Controller } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { z } from "zod";
+import Benefit from "./Benefit";
 
-const formSchema = z.object({});
+const formSchema = z.object({
+  description: z.string().nonempty("Description is required"),
+  vision: z.string().nonempty("Vision is required"),
+  website: z.string().nonempty("Website is required"),
+  founded: z.string().nonempty("Founded date is required"),
+  organization_type: z.string().nonempty("Organization type is required"),
+  industry_type: z.string().nonempty("Industry type is required"),
+  team_size: z
+    .number()
+    .nonnegative("Team size must be a positive number")
+    .int("Team size must be an integer"),
+});
 
 const Founding = () => {
   const form = useForm({
     resolver: zodResolver(formSchema),
     defaultValues: {
       description:
-        "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta acconsectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel",
-      benefits:
         "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta acconsectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel",
       vision:
         "Cras mattis consectetur purus sit amet fermentum. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta acconsectetur ac, vestibulum at eros. Praesent commodo cursus magna, vel",
@@ -29,31 +39,28 @@ const Founding = () => {
   };
 
   return (
-    <Box
-      component="form"
-      onSubmit={form.handleSubmit(onSubmit)}
-      sx={{ width: "100%", py: 10 }}
-    >
-      <Typography sx={{ fontWeight: "bold" }} gutterBottom>
-        DESCRIPTION
-      </Typography>
-      <Controller
-        name="description"
-        control={form.control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            margin="dense"
-            label="Description"
-            size="small"
-            multiline
-            error={!!form.formState.errors.description}
-            helperText={form.formState.errors.description?.message}
-            sx={{ width: "100%" }}
-          />
-        )}
-      />
-      <Typography sx={{ fontWeight: "bold", mt: 4 }} gutterBottom>
+    <Box sx={{ width: "100%", py: 10 }}>
+      <Box component="form" onSubmit={form.handleSubmit(onSubmit)}>
+        <Typography sx={{ fontWeight: "bold" }} gutterBottom>
+          DESCRIPTION
+        </Typography>
+        <Controller
+          name="description"
+          control={form.control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              margin="dense"
+              label="Description"
+              size="small"
+              multiline
+              error={!!form.formState.errors.description}
+              helperText={form.formState.errors.description?.message}
+              sx={{ width: "100%" }}
+            />
+          )}
+        />
+        {/* <Typography sx={{ fontWeight: "bold", mt: 4 }} gutterBottom>
         Benefits
       </Typography>
 
@@ -72,144 +79,153 @@ const Founding = () => {
             sx={{ width: "100%" }}
           />
         )}
-      />
+      /> */}
 
-      <Typography sx={{ fontWeight: "bold", mt: 4 }} gutterBottom>
-        Vision
-      </Typography>
+        <Typography sx={{ fontWeight: "bold", mt: 4 }} gutterBottom>
+          Vision
+        </Typography>
 
-      <Controller
-        name="vision"
-        control={form.control}
-        render={({ field }) => (
-          <TextField
-            {...field}
-            margin="dense"
-            label="Vision"
-            size="small"
-            multiline
-            error={!!form.formState.errors.vision}
-            helperText={form.formState.errors.vision?.message}
-            sx={{ width: "100%" }}
-          />
-        )}
-      />
+        <Controller
+          name="vision"
+          control={form.control}
+          render={({ field }) => (
+            <TextField
+              {...field}
+              margin="dense"
+              label="Vision"
+              size="small"
+              multiline
+              error={!!form.formState.errors.vision}
+              helperText={form.formState.errors.vision?.message}
+              sx={{ width: "100%" }}
+            />
+          )}
+        />
 
-      <Box sx={{ my: 4 }}>
-        <Box sx={{ width: "100%" }}>
-          <Typography variant="h6" gutterBottom>
-            Website
-          </Typography>
-          <Controller
-            name="website"
-            control={form.control}
-            render={({ field }) => (
-              <TextField
-                {...field}
-                margin="dense"
-                label="Website"
-                size="small"
-                error={!!form.formState.errors.website}
-                helperText={form.formState.errors.website?.message}
-                sx={{ width: "100%" }}
+        <Box sx={{ my: 4 }}>
+          <Box sx={{ width: "100%" }}>
+            <Typography variant="h6" gutterBottom>
+              Website
+            </Typography>
+            <Controller
+              name="website"
+              control={form.control}
+              render={({ field }) => (
+                <TextField
+                  {...field}
+                  margin="dense"
+                  label="Website"
+                  size="small"
+                  error={!!form.formState.errors.website}
+                  helperText={form.formState.errors.website?.message}
+                  sx={{ width: "100%" }}
+                />
+              )}
+            />
+          </Box>
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Box sx={{ width: "100%" }}>
+              <Typography variant="h6" gutterBottom>
+                Founded
+              </Typography>
+              <Controller
+                name="founded"
+                control={form.control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    margin="dense"
+                    label="Founded"
+                    size="small"
+                    error={!!form.formState.errors.founded}
+                    helperText={form.formState.errors.founded?.message}
+                    sx={{ width: "100%" }}
+                  />
+                )}
               />
-            )}
-          />
-        </Box>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <Box sx={{ width: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              Founded
-            </Typography>
-            <Controller
-              name="founded"
-              control={form.control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="dense"
-                  label="Founded"
-                  size="small"
-                  error={!!form.formState.errors.founded}
-                  helperText={form.formState.errors.founded?.message}
-                  sx={{ width: "100%" }}
-                />
-              )}
-            />
+            </Box>
+
+            <Box sx={{ width: "100%" }}>
+              <Typography variant="h6" gutterBottom>
+                Team_size:
+              </Typography>
+              <Controller
+                name="team_size"
+                control={form.control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    margin="dense"
+                    label="Team size"
+                    size="small"
+                    error={!!form.formState.errors.team_size}
+                    helperText={form.formState.errors.team_size?.message}
+                    sx={{ width: "100%" }}
+                  />
+                )}
+              />
+            </Box>
+          </Box>
+          <Box sx={{ display: "flex", gap: 2 }}>
+            <Box sx={{ width: "100%" }}>
+              <Typography variant="h6" gutterBottom>
+                Organization Type
+              </Typography>
+              <Controller
+                name="organization_type"
+                control={form.control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    margin="dense"
+                    label="Organization type"
+                    size="small"
+                    error={!!form.formState.errors.organization_type}
+                    helperText={
+                      form.formState.errors.organization_type?.message
+                    }
+                    sx={{ width: "100%" }}
+                  />
+                )}
+              />
+            </Box>
+
+            <Box sx={{ width: "100%" }}>
+              <Typography variant="h6" gutterBottom>
+                Industry Type:
+              </Typography>
+              <Controller
+                name="industry_type"
+                control={form.control}
+                render={({ field }) => (
+                  <TextField
+                    {...field}
+                    margin="dense"
+                    label="Industry type"
+                    size="small"
+                    error={!!form.formState.errors.industry_type}
+                    helperText={form.formState.errors.industry_type?.message}
+                    sx={{ width: "100%" }}
+                  />
+                )}
+              />
+            </Box>
           </Box>
 
-          <Box sx={{ width: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              Team_size:
-            </Typography>
-            <Controller
-              name="team_size"
-              control={form.control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="dense"
-                  label="Team size"
-                  size="small"
-                  error={!!form.formState.errors.team_size}
-                  helperText={form.formState.errors.team_size?.message}
-                  sx={{ width: "100%" }}
-                />
-              )}
-            />
-          </Box>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{ textTransform: "none", width: "100%", mt: 1 }}
+          >
+            Save Chanages
+          </Button>
         </Box>
-        <Box sx={{ display: "flex", gap: 2 }}>
-          <Box sx={{ width: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              Organization Type
-            </Typography>
-            <Controller
-              name="organization_type"
-              control={form.control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="dense"
-                  label="Organization type"
-                  size="small"
-                  error={!!form.formState.errors.organization_type}
-                  helperText={form.formState.errors.organization_type?.message}
-                  sx={{ width: "100%" }}
-                />
-              )}
-            />
-          </Box>
+      </Box>
 
-          <Box sx={{ width: "100%" }}>
-            <Typography variant="h6" gutterBottom>
-              Industry Type:
-            </Typography>
-            <Controller
-              name="industry_type"
-              control={form.control}
-              render={({ field }) => (
-                <TextField
-                  {...field}
-                  margin="dense"
-                  label="Industry type"
-                  size="small"
-                  error={!!form.formState.errors.industry_type}
-                  helperText={form.formState.errors.industry_type?.message}
-                  sx={{ width: "100%" }}
-                />
-              )}
-            />
-          </Box>
-        </Box>
+      <Divider />
 
-        <Button
-          type="submit"
-          variant="contained"
-          sx={{ textTransform: "none", width: "100%", mt: 1 }}
-        >
-          Save Chanages
-        </Button>
+      <Box>
+        <Benefit />
       </Box>
     </Box>
   );
