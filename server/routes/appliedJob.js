37c44245173e441f20ec9fbb07530302b904085b
NuwanSD <sagaradnuwan@gmail.com>
2025-02-23 04:30:23 +0530

    route id updated

--- server/routes/appliedJob.js
+++ server/routes/appliedJob.js
@@ -4,9 +4,9 @@ const AppliedJobController = require("../controllers/appliedJob");
 const router = express.Router();
 
 router.get("/applied_job", AppliedJobController.getAppliedJobs);
-router.get("/applied_job/:pid/:sid", AppliedJobController.getAppliedJobById);
+router.get("/applied_job/:pid/:uid", AppliedJobController.getAppliedJobById);
 router.post("/applied_job", AppliedJobController.saveAppliedJob);
-router.delete("/applied_job/:pid/:sid", AppliedJobController.deleteAppliedJob);
-router.patch("/applied_job/:pid/:sid", AppliedJobController.updateAppliedJob);
+router.delete("/applied_job/:pid/:uid", AppliedJobController.deleteAppliedJob);
+router.patch("/applied_job/:pid/:uid", AppliedJobController.updateAppliedJob);
 
 module.exports = router;
