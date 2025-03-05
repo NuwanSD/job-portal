29a8de013d115b4fb3f8e9cdf6de685a59dfbb3a
NuwanSD <sagaradnuwan@gmail.com>
2025-03-05 15:07:18 +0530

    updated and seperate the routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,12 @@
 import React from "react";
-import AuthProvider from "./provider/AuthProvider";
-import Routes from "./routes";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
+import { BrowserRouter as Router } from "react-router-dom";
+import AppRoutes from "./routes";
 
 const App = () => {
   return (
-    <AuthProvider>
-      <div id="root">
-        <div className="main">
-          <Navbar />
-          <Routes />
-        </div>
-        <Footer />
-      </div>
-    </AuthProvider>
+    <Router>
+      <AppRoutes />
+    </Router>
   );
 };
 
