89c9d198cffd1ee870c2abd84829b39cfb5b3591
NuwanSD <sagaradnuwan@gmail.com>
2025-03-08 12:10:52 +0530

    added data fetching functions

--- client/src/pages/home/Companies.jsx
+++ client/src/pages/home/Companies.jsx
@@ -1,4 +1,4 @@
-import * as React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
@@ -6,81 +6,41 @@ import Typography from "@mui/material/Typography";
 import { Button } from "@mui/material";
 import LocationOnOutlinedIcon from "@mui/icons-material/LocationOnOutlined";
 import Logo from "../../assets/facebook.svg";
-
-const companies = [
-  {
-    id: 1,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 2,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 3,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 4,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 5,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 6,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 7,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-  {
-    id: 8,
-    name: "Facebook",
-    location: "Perth, Australia",
-    available: 4,
-    logo: Logo,
-  },
-];
+import { getAllUsers } from "../../helper/helper";
 
 function Companies() {
+  const [recruiter, setRecruiter] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const users = await getAllUsers();
+
+        const findRecruiters = users.data.filter((u) => u.role === "recruiter");
+
+        setRecruiter(findRecruiters);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <Box
       sx={{
         width: "100%",
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fill, minmax(min(200px, 100%), 1fr))",
-        gap: 5,
+        gridTemplateColumns: "repeat(auto-fill, minmax(min(250px, 100%), 1fr))",
+        gap: 3,
       }}
     >
-      {companies.map((company) => (
-        <Card key={company.id} variant="outlined" sx={{ borderRadius: 2 }}>
+      {recruiter.map((company) => (
+        <Card key={company.user_id} variant="outlined" sx={{ borderRadius: 2 }}>
           <CardContent sx={{ height: "100%" }}>
             <Box sx={{ display: "flex", gap: 1, alignItems: "center" }}>
-              <img src={company.logo} alt="" width={82} />
+              <img src={Logo} alt="" width={82} />
               <Box>
                 <Typography variant="h6" component="div" sx={{ ml: 0.5 }}>
                   {company.name}
@@ -91,12 +51,16 @@ function Companies() {
                   sx={{ display: "flex", alignItems: "center" }}
                 >
                   <LocationOnOutlinedIcon />
-                  {company.location}
+                  {company.city}, {company.country}
                 </Typography>
               </Box>
             </Box>
-            <Button variant="outlined" sx={{ mt: 2, width: "100%" }}>
-              Open Position ({company.available})
+            <Button
+              variant="outlined"
+              sx={{ mt: 2, width: "100%" }}
+              href={`/recruiters/${company.user_id}`}
+            >
+              Open Position
             </Button>
           </CardContent>
         </Card>
